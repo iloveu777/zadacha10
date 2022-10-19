2bd94c1084419ebf8860d2e55c017b134d97eb11
iloveu777 <zuxplil1337@yandex.ru>
2022-10-19 22:34:00 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            int b;
+            Write("Как тебя зовут? ");
+            a = Convert.ToString(ReadLine());
+            Write("Сколько тебе лет?");
+            b = Convert.ToInt32(ReadLine());
+            int c = DateTime.Today.Year;
+            WriteLine($"{a}, ты родился в {c - b}  году");
+            ReadKey();
+        }
+    }
+}
