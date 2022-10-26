857d4e7805d487fed1ec9add905cce36e29544cc
iloveu777 <zuxplil1337@yandex.ru>
2022-10-26 08:04:40 +0300

    Update Program.cs
    123

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using static System.Console;
 
@@ -9,17 +10,27 @@ namespace ConsoleApp3
 {
     internal class Program
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
+            SetWindowSize(WindowWidth, 71);
+            SetWindowSize(WindowHeight, 71);
+            BackgroundColor = ConsoleColor.White;
+            ForegroundColor = ConsoleColor.Black;
+            Clear();
+            Title = "123";
+            int height = WindowHeight;
+            int width = WindowWidth;
+            WriteLine($"высота {height}, ширина {width}");
             string a;
             int b;
             Write("Как тебя зовут? ");
             a = Convert.ToString(ReadLine());
-            Write("Сколько тебе лет?");
+            Write("Сколько тебе лет? ");
             b = Convert.ToInt32(ReadLine());
             int c = DateTime.Today.Year;
             WriteLine($"{a}, ты родился в {c - b}  году");
             ReadKey();
+            
         }
     }
 }
